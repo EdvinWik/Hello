712d940eddb9989c43b7e5b5719c113aab2d83a8
EdvinWik <edvin.wik@elev.ga.ntig.se>
2021-09-13 13:12:48 +0200

    ✨ mmmmm

--- test.py
+++ test.py
@@ -5,3 +5,19 @@ turtle.turtlesize = 10
 turtle.color("Red")
 turtle.circle(75)
 turtle.circle(-75)
+
+tal3 = 12
+tal2 = float(input("Skriv ett tal"))
+
+summa = tal3 * tal2
+
+print(f"Summan blir {summa}")
+
+then = summa = tal3 + tal2
+do = print(f"Summan blir {summa}")
+ 
+then = summa = tal3 / tal2
+do = print(f"Summan blir {summa}")
+
+then = summa = tal3 - tal2
+do = print(f"Summan blir {summa}")
